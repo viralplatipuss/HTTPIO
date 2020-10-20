db9fe6ab4324b37f1389bcfbe67c540b4747b1ef
Dom Chapman <viralplatipuss@gmail.com>
2020-10-20 21:21:18 +0100

    New Request struct

--- Sources/HTTPIO/HTTPIO.swift
+++ Sources/HTTPIO/HTTPIO.swift
@@ -4,23 +4,33 @@ import SimpleFunctional
 /// IO type for making simple HTTP requests.
 public struct HTTPIO: IO {
     
-    public enum Input {
-        public enum RequestResult {
+    public struct Input {
+        public enum Result {
             case failed(errorMessage: String)
             case responded(httpStatusCode: UInt, bodyBytes: [UInt8])
         }
         
-        case requestStarted(id: UInt, hostname: String, path: String)
-        case requestEnded(id: UInt, result: RequestResult)
+        public enum Update {
+            case requestStarted(Request)
+            case requestEnded(result: Result)
+        }
+        
+        public let id: UInt
+        public let update: Update
     }
     
     public enum Output {
-        /// Make an HTTP request to download bytes.
+        /// Make an HTTP request.
         /// This will always produce a requestStarted input. The id of which can be used to identify the response later.
-        case requestBytes(hostname: String, path: String)
+        case request(Request)
         
         /// Attempts to forcefully end an existing HTTP connection, if it exists for the given id.
         /// If the connection was able to be force ended, this will be a failure input result.
         case forceEndRequest(id: UInt)
+    }
+    
+    public struct Request: Hashable {
+        public let hostname: String
+        public let path: String
     }
 }
--- Sources/HTTPIO/HTTPIOHandler.swift
+++ Sources/HTTPIO/HTTPIOHandler.swift
@@ -11,7 +11,7 @@ public final class HTTPIOHandler: BaseIOHandler<HTTPIO> {
     
     public override func handle(output: Output) {
         switch output {
-        case let .requestBytes(hostname, path): downloadBytes(hostname: hostname, path: path)
+        case let .request(req): downloadBytes(request: req)
         case let .forceEndRequest(id): forceEnd(id: id)
         }
     }
@@ -23,28 +23,28 @@ public final class HTTPIOHandler: BaseIOHandler<HTTPIO> {
     private var nextId: UInt = 0
     private var clientFutureForId = [UInt: EventLoopFuture<HTTPClient>]()
     
-    private func downloadBytes(hostname: String, path: String) {
+    private func downloadBytes(request: HTTPIO.Request) {
         let id = nextId
         nextId += 1
         
-        runInput(.requestStarted(id: id, hostname: hostname, path: path))
+        runInput(.init(id: id, update: .requestStarted(request)))
         
-        guard let url = URL(string: path) else {
-            runInput(.requestEnded(id: id, result: .failed(errorMessage: "Invalid path: \(path)")))
+        guard let url = URL(string: request.path) else {
+            runInput(.init(id: id, update: .requestEnded(result: .failed(errorMessage: "Invalid path: \(request.path)"))))
             return
         }
         
-        let clientFuture = HTTPClient.connect(hostname: hostname, on: worker)
+        let clientFuture = HTTPClient.connect(hostname: request.hostname, on: worker)
         clientFutureForId[id] = clientFuture
         
         let httpResFuture = clientFuture.flatMap(to: HTTPResponse.self) { $0.send(HTTPRequest(url: url)) }
         
         httpResFuture.whenSuccess { [weak self] in
-            self?.runInput(.requestEnded(id: id, result: .responded(httpStatusCode: $0.status.code, bodyBytes: $0.body.data.map { [UInt8]($0) } ?? [])))
+            self?.runInput(.init(id: id, update: .requestEnded(result: .responded(httpStatusCode: $0.status.code, bodyBytes: $0.body.data.map { [UInt8]($0) } ?? []))))
         }
         
         httpResFuture.whenFailure { [weak self] in
-            self?.runInput(.requestEnded(id: id, result: .failed(errorMessage: $0.localizedDescription)))
+            self?.runInput(.init(id: id, update: .requestEnded(result: .failed(errorMessage: $0.localizedDescription))))
         }
         
         httpResFuture.always { [weak self] in
@@ -57,7 +57,7 @@ public final class HTTPIOHandler: BaseIOHandler<HTTPIO> {
         
         let closeFuture = clientFuture.flatMap { $0.close() }
         closeFuture.whenSuccess { [weak self] in
-            self?.runInput(.requestEnded(id: id, result: .failed(errorMessage: "Connection was force ended.")))
+            self?.runInput(.init(id: id, update: .requestEnded(result: .failed(errorMessage: "Connection was force ended."))))
         }
     }
 }
